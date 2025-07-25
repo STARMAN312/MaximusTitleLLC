e9540941f4d8441298b5d69678d904285e37b646
star man <irvinarielmadrid@gmail.com>
2025-07-25 08:59:04 -0700

    Add project files.

---
+++ MaximusTitleLLC/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+}
---
+++ MaximusTitleLLC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MaximusTitleLLC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
